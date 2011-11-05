1d11d204fde76c91e09ace312bbbf390f22f6214
tobias.deb@gmail.com <tobias.deb@gmail.com@de8ff2d8-50e4-f369-dfdf-c7f353701c3e>
2011-11-05 20:28:13 +0000

    SCENER: help lib

---
+++ src/scener/sdltools.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+
+import pygame
+from pygame.locals import *
+
+def load_image(fullname):
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', fullname
+        raise SystemExit, message
+    return image.convert_alpha()
+
+def get_filenames(basename):
+    for frame_n in range(1, 100):
+        filename = basename + '%02d' % frame_n + '.png'
+        yield filename
+
+def vflip_frame(frame):
+    return pygame.transform.flip(frame, 1, 0)
+
+def hflip_frame(frame):
+    return pygame.transform.flip(frame, 0, 1)
